7e5f4302d6930f7f60504c68271b32083cebbe93
Ilya Gordeev <bigdirtyrussian@gmail.com>
2023-11-13 15:52:20 +0300

    123

--- test_items.py
+++ test_items.py
@@ -2,16 +2,16 @@ from selenium.webdriver.common.by import By
 import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-# time.sleep(30)
+
 
 def test_guest_should_see_login_link(browser):
     browser.get(link)
+    time.sleep(30)
     browser.find_element(By.CSS_SELECTOR, "#login_link")
 
 def find_add_to_basket_button(browser):
     browser.get(link)
-    find_button = browser.find_element(By.CLASS_NAME, 'btn.btn-lg.btn-primary.btn-add-to-basket')
+    find_button = browser.find_elements(By.CLASS_NAME, 'btn.btn-lg.btn-primary.btn-add-to-basket')
     find_button.click()
     print('Button finded:', find_button)
-    assert find_button in None
-time.sleep(30)
+    assert find_button in None, 'Button not found'
