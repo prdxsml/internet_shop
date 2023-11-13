c06e660b7e3d62e1295bde0c9463abf1081831a5
Ilya Gordeev <bigdirtyrussian@gmail.com>
2023-11-13 15:38:16 +0300

    Запуск автотестов для разных языков интерфейса

--- conftest.py
+++ conftest.py
@@ -4,36 +4,22 @@ from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     # Выбор браузера для теста
-    parser.addoption('--browser_name', action='store', default=None, help="Choose browser: chrome or firefox")
+    parser.addoption('--browser_name', action='store', default=None, help="chrome")
     # Обработчик командной строки для выбора языка теста. Представлены все тестируемые локали.
     parser.addoption('--language', action='store', default='en', help='What language will we use for the test? Languages available for verification: ar = العربيّة, ca = català, cs = česky, da = dansk, de = Deutsch, en-gb = British English, el = Ελληνικά, es = español, fi = suomi, fr = français, it = italiano, ko = 한국어, nl = Nederlands, pl = polski, pt = Português, pt-br = Português Brasileiro, ro = Română, ru = Русский, sk = Slovensky, uk = Українська, zh-hans = 简体中文')
 
 
-
-# @pytest.fixture
-# def cmdopt(request):
-#     return request.config.getoption("--cmdopt")
-
-# @pytest.fixture(scope="function")
 def language(request):
     return request.config.getoption("--language")
-    # language = request.config.getoption("language")
-    # language = None
-
-
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
     browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test ...")
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test ...")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    browser_name == "chrome"
+    print("\nstart chrome browser for test ...")
+    browser = webdriver.Chrome()
+
     
     yield browser
     print("\nquit browser..")
--- test_items.py
+++ test_items.py
@@ -1,7 +1,14 @@
 from selenium.webdriver.common.by import By
 
-link = "http://selenium1py.pythonanywhere.com/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_login_link(browser):
     browser.get(link)
     browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+def find_add_to_basket_button(browser):
+    browser.get(link)
+    find_button = browser.find_element(By.CLASS_NAME, 'btn.btn-lg.btn-primary.btn-add-to-basket')
+    find_button.click()
+    print('Button finded:', find_button)
+    assert find_button in None
